e17a9d2dc1b6e55b640cfb3e3e7e338bc5152ba9
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-08-20 10:56:56 +0200

    create header, search and context for search

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,136 @@
 "use client";
 
+import Image from "next/image";
+import { useContext, useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { Link } from "react-scroll";
+import { BiMenuAltRight, BiX } from "react-icons/bi";
+import SearchMobile from "./SearchMobile";
+import { SearchContext } from "@/context/search";
+
 const Header = () => {
-  return <div></div>;
+  const { setSearchActive } = useContext(SearchContext);
+
+  const [header, setHeader] = useState(false);
+  const [nav, setNav] = useState(false);
+
+  const descktopMode = useMediaQuery({
+    query: "(min-width: 1300px)",
+  });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 40 ? setHeader(true) : setHeader(false);
+      window.scrollY > 800 ? setSearchActive(true) : setSearchActive(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <header
+      className={`${
+        header ? "bg-white shadow-md py-2" : "bg-transparent shadow-none py-4"
+      } fixed w-full max-w-[1920px] mx-auto z-20 transition-all duration-300`}
+    >
+      <div className="xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-between">
+        <div className="flex justify-between items-cente px-4">
+          <Link
+            to="home"
+            smooth={descktopMode}
+            spy={true}
+            className="cursor-pointer"
+          >
+            <Image src={"/icons/logo.svg"} width={194} alt="Logo" height={64} />
+          </Link>
+          <div
+            onClick={() => setNav(!nav)}
+            className="cursor-pointer xl:hidden"
+          >
+            {nav ? (
+              <BiX className="text-4xl" />
+            ) : (
+              <BiMenuAltRight className="text-4xl" />
+            )}
+          </div>
+        </div>
+        <nav
+          className={`${
+            nav
+              ? "max-h-max py-8 px-4 xl: py-0 xl: px-0"
+              : "max-h-0 xl:max-h-max"
+          } flex flex-col w-full bg-white gap-y-6 overflow-hidden font-bolt xl:font-medium xl:flex-row xl:w-max xl:gap-x-8 xl:h-max xl:bg-transparent xl:pb-0 transition-all duration-150 text-center xl:text-left  uppercase text-sm xl:text-[15px] xl:normal-case`}
+        >
+          <Link
+            className="cursor-pointer"
+            to="home"
+            activeClass="active"
+            smooth={descktopMode}
+            spy={true}
+          >
+            Home
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="cars"
+            activeClass="active"
+            smooth={descktopMode}
+            spy={true}
+          >
+            Cars
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="about"
+            activeClass="active"
+            smooth={descktopMode}
+            spy={true}
+          >
+            About
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="why"
+            activeClass="active"
+            smooth={descktopMode}
+            spy={true}
+          >
+            Why us
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="testimonials"
+            activeClass="active"
+            smooth={descktopMode}
+            spy={true}
+          >
+            Testimonials
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="contact"
+            activeClass="active"
+            smooth={descktopMode}
+            spy={true}
+          >
+            Contact
+          </Link>
+          <Link
+            className="xl:hidden btn btn-primary btn-sm max-w-[164px] mx-auto"
+            to="/"
+            activeClass="active"
+            smooth={descktopMode}
+            spy={true}
+          >
+            See all cars
+          </Link>
+          <SearchMobile />
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Search.jsx
+++ components/Search.jsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { SearchContext } from "@/context/search";
+import { useContext } from "react";
+import LocationSelection from "./LocationSelection";
+
 const Search = () => {
-  return <div>Search</div>;
+  const { searchActive } = useContext(SearchContext);
+  return (
+    <div
+      className={`${
+        searchActive
+          ? "bg-white rounded-none xl:-[98px] "
+          : "bg-white rounden-[20px] py-6 xl:r-4 xl:h-[98ox]"
+      } hidden xl:blck w-full relative shadow-lg`}
+    >
+      <div className="xl:h-full flex items-center px-6 xl:px-0">
+        <LocationSelection />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
