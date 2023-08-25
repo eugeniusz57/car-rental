336eb632403f7d3e20e24f054aec639adcb347bc
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-08-25 17:13:36 +0200

    create brand section and start creating section carsSlider

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -1,5 +1,72 @@
+"use client";
+
+import Image from "next/image";
+
 const Brands = () => {
-  return <div>Brands</div>;
+  return (
+    <section className="xl:pt-16 xl:h-{200px] bg-white flex flex-col justify-center">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-6 place-items-center xl:flex xl:flex-wrap xl:gap-x-6 xl:justify-between">
+          <div>
+            <Image
+              src={"icons/brands/ford.svg"}
+              width={85}
+              height={32}
+              alt="icon brand"
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/mercedes.svg"}
+              width={60}
+              height={60}
+              alt="icon brand"
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/audi.svg"}
+              width={85}
+              height={50}
+              alt="icon brand"
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/bmw.svg"}
+              width={60}
+              height={60}
+              alt="icon brand"
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/vw.svg"}
+              width={60}
+              height={60}
+              alt="icon brand"
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/skoda.svg"}
+              width={85}
+              height={32}
+              alt="icon brand"
+            />
+          </div>
+          <div>
+            <Image
+              src={"icons/brands/mazda.svg"}
+              width={62}
+              height={50}
+              alt="icon brand"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Brands;
--- components/CarSlider.jsx
+++ components/CarSlider.jsx
@@ -1,5 +1,145 @@
+"use client";
+
+import "swiper/css";
+
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const carsArray = [
+  {
+    type: "Hatchback",
+    name: "Ford Focus",
+    price: 29,
+    stars: 3.5,
+    image: "images/carSlider/car01.svg",
+    info: [
+      {
+        icon: "icons/carSlider/gearshift.svg",
+        text: "Manual",
+      },
+      {
+        icon: "icons/carSlider/seat.svg",
+        text: "5 Seats",
+      },
+      {
+        icon: "icons/carSlider/gas.svg",
+        text: "Gas",
+      },
+      {
+        icon: "icons/carSlider/engine.svg",
+        text: "1600 HP",
+      },
+      {
+        icon: "icons/carSlider/wheel.svg",
+        text: "Front",
+      },
+    ],
+  },
+  {
+    type: "Sedan",
+    name: "Toyota Corolla",
+    price: 25,
+    stars: 5,
+    image: "images/carSlider/car02.svg",
+    info: [
+      {
+        icon: "icons/carSlider/gearshift.svg",
+        text: "Manual",
+      },
+      {
+        icon: "icons/carSlider/seat.svg",
+        text: "5 Seats",
+      },
+      {
+        icon: "icons/carSlider/gas.svg",
+        text: "Gas",
+      },
+      {
+        icon: "icons/carSlider/engine.svg",
+        text: "1600 HP",
+      },
+      {
+        icon: "icons/carSlider/wheel.svg",
+        text: "Front",
+      },
+    ],
+  },
+  {
+    type: "SUV",
+    name: "Honda CR-V",
+    price: 35,
+    stars: 4.7,
+    image: "images/carSlider/car03.svg",
+    info: [
+      {
+        icon: "icons/carSlider/gearshift.svg",
+        text: "Automatic",
+      },
+      {
+        icon: "icons/carSlider/seat.svg",
+        text: "5 Seats",
+      },
+      {
+        icon: "icons/carSlider/gas.svg",
+        text: "Gas",
+      },
+      {
+        icon: "icons/carSlider/engine.svg",
+        text: "1600 HP",
+      },
+      {
+        icon: "icons/carSlider/wheel.svg",
+        text: "Front",
+      },
+    ],
+  },
+  {
+    type: "Convertible",
+    name: "Mazda MX-5",
+    price: 32,
+    stars: 4.9,
+    image: "images/carSlider/car02.svg",
+    info: [
+      {
+        icon: "icons/carSlider/gearshift.svg",
+        text: "Manual",
+      },
+      {
+        icon: "icons/carSlider/seat.svg",
+        text: "5 Seats",
+      },
+      {
+        icon: "icons/carSlider/gas.svg",
+        text: "Gas",
+      },
+      {
+        icon: "icons/carSlider/engine.svg",
+        text: "1600 HP",
+      },
+      {
+        icon: "icons/carSlider/wheel.svg",
+        text: "Front",
+      },
+    ],
+  },
+];
+
 const CarSlider = () => {
-  return <div>CarSlider</div>;
+  return (
+    <div className="container mx-auto">
+      <Swiper
+        breakpoints={{
+          320: { slidesPerView: 1, spaceBetween: 15 },
+          640: { slidesPerView: 2, spaceBetween: 32 },
+          1260: { slidesPerView: 3, spaceBetween: 32 },
+        }}
+      >
+        {carsArray.map((car, index) => {
+          return <SwiperSlide key={index}>slide</SwiperSlide>;
+        })}
+      </Swiper>
+    </div>
+  );
 };
 
 export default CarSlider;
