a4a38910abee9c9b1dccbf3b6305a2e9bfcc6989
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-08-28 18:29:54 +0200

    create testim. slider with swiper

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,7 +1,11 @@
+import TestimonialSlider from "./TestimonialSlider";
+
 const Testimonial = () => {
   return (
-    <section className="section flex items-center bg-cyan-300" id="testimonial">
-      Testimonial
+    <section className="section flex items-center " id="testimonial">
+      <div className="container mx-auto">
+        <TestimonialSlider />
+      </div>
     </section>
   );
 };
