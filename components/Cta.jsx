1506fa673be8e631b46d0fd946ac5c303f710276
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-09-05 17:27:49 +0200

    create section contact, add style and animation

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -1,10 +1,80 @@
+"use client";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/variant";
+import Image from "next/image";
 const Cta = () => {
   return (
     <section
       className="pt-24 xl:pt-48 flex items-end pb-0 bg-[#b2b7c2]/10"
       id="contact"
     >
-      Cta
+      <div className="container mx">
+        <div className="flex flex-col md:flex-row xl:items-center">
+          <div className="flex-1 xl:ml-24 text-center md:text-left mb-12 md:mb-0">
+            <div className="max-w-[520px] mx-auto order-2 xl:order-none">
+              <motion.h2
+                className="h2"
+                variants={fadeIn("up", 0.2)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.6 }}
+              >
+                Download our App now and hit the road with ease
+              </motion.h2>
+              <motion.p
+                variants={fadeIn("up", 0.4)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.6 }}
+                className="mb-10"
+              >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Repellendus dolores eveniet nesciunt vel nulla. Consectetur odio
+                molestiae harum aspernatur voluptas perspiciatis doloremque
+                porro. Magni molestias quo error labore modi consequatur!
+              </motion.p>
+              <motion.div
+                className="flex gap-x-3 justify-center md:justify-start"
+                variants={fadeIn("left", 0.6)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.6 }}
+              >
+                <button className="btn-cta">
+                  <Image
+                    src={"/icons/buttons/google-play.svg"}
+                    width={132}
+                    height={36}
+                    alt="button icon"
+                  />
+                </button>
+                <button className="btn-cta">
+                  <Image
+                    src={"/icons/buttons/app-store.svg"}
+                    width={132}
+                    height={36}
+                    alt="button icon"
+                  />
+                </button>
+              </motion.div>
+            </div>
+          </div>
+          <motion.div
+            className="flex flex-1 justify-center order-1 md:order-none"
+            variants={fadeIn("up", 0.8)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.2 }}
+          >
+            <Image
+              src={"/images/cta/phone.svg"}
+              width={320}
+              height={640}
+              alt="image phone"
+            />
+          </motion.div>
+        </div>
+      </div>
     </section>
   );
 };
