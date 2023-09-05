f525c3296b5c0206490ac68eea73567a03db21a9
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-09-05 18:57:53 +0200

    add button up

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -4,7 +4,10 @@ import { fadeIn } from "@/variant";
 import Image from "next/image";
 const Cta = () => {
   return (
-    <section className="pt-24 xl:pt-48 flex items-end pb-0 " id="contact">
+    <section
+      className="pt-24 xl:pt-48 flex items-end pb-0 bg-[#b2b7c2]/10 overflow-hidden"
+      id="contact"
+    >
       <div className="container mx">
         <div className="flex flex-col md:flex-row xl:items-center">
           <div className="flex-1 xl:ml-24 text-center md:text-left mb-12 md:mb-0">
